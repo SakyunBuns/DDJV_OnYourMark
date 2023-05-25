153067cbc35a9631cbc141c6d27ccfb576c85304
QHT <quoc.huan.tran@gmail.com>
2023-05-25 15:56:08 -0400

    Update script for fruit to make it fade out and destroy Adding side wall to stage 1

--- Assets/Script/Fruit.cs
+++ Assets/Script/Fruit.cs
@@ -7,19 +7,30 @@ public class Fruit: MonoBehaviour
     private Animator animator;
     private string[] fruitAnimations;
     private float speed = 2.5f;
-
+    private bool deleteMe = false; 
 
     void Start()
     {   
         fruitAnimations = new string[]{"banana", "anana", "fraise", "melon", "kiwi", "orange", "cerise"};
         animator = GetComponent<Animator>();
         PlayRandomAnimation();
+        StartCoroutine(RemoveMe());
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (deleteMe)
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            Color color = spriteRenderer.color;
+            color.a -= Time.fixedDeltaTime;
+            spriteRenderer.color = color;
+
+            if (color.a <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     void PlayRandomAnimation()
@@ -32,5 +43,10 @@ public class Fruit: MonoBehaviour
     {
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         rigidbody.velocity = direction.normalized * speed;
+    }
+
+    private IEnumerator RemoveMe(){
+        yield return new WaitForSeconds(3.0f);
+        deleteMe = true;
     }
 }
